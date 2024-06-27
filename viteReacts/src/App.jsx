197ddaed6fc79017c52de13bbc71c220e0d46631
Jaipal Singh Sisodiya <jaipalsinghsisodiya@gmail.com>
2024-06-27 12:08:07 +0530

    Create Custom ReactJs

--- viteReacts/src/App.jsx
+++ viteReacts/src/App.jsx
@@ -3,10 +3,13 @@ import Chai from "./chai"
 
 function App() {
   
-
+const username = "Hitesh Choudary"
   return (
-    
+    <>
   <Chai/>
+  <h1>Chai aur React {username} </h1>
+  <p>Test para</p>
+  </>
   )
 }
 
--- viteReacts/src/main.jsx
+++ viteReacts/src/main.jsx
@@ -3,8 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com ",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit google</a>
+)
+
+const anotherUser = "Chai or react"
+
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target:'_blank'},'Click me to visit google', anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  // <App/>
+reactElement
 )
