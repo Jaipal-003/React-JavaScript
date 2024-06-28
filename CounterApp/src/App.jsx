af2644bd9cdd6f399d0f062b6fcaa3177a75022c
Jaipal Singh Sisodiya <jaipalsinghsisodiya@gmail.com>
2024-06-28 11:29:34 +0530

    CounterApp Complete

---
+++ CounterApp/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+
+let [counter,setCounter] = useState(0)
+
+  //let counter = 0;
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Chai aur React</h1>
+      <h3>Counter value: {counter}</h3>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  );
+}
+
+export default App;
