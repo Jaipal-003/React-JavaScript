74a4b342a80983ebf8e3d395366aaa3d711eb42a
Jaipal Singh Sisodiya <jaipalsinghsisodiya@gmail.com>
2024-07-18 14:08:56 +0530

    PasswordGenerator

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 
-
 function App() {
-  const [length, setLength] = useState(10);
+  const [length, setlength] = useState(8);
   const [numberAllowed, setnumberAllowed] = useState(false);
-  const [charAllowed, setcharAllowed] = useState(false);
-  const [password, setpassword] = useState("");
+  const [charAllowed, setcharAllowed] = useState(false); 
+  const [password, setpassword] = useState(false); 
 
-  const passwordGenerator = () => {};
 
   return (
     <>
-      
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
     </>
   );
 }
