885718283441584217cd04853ca85d5715acb946
Jaipal Singh Sisodiya <jaipalsinghsisodiya@gmail.com>
2024-07-27 23:29:06 +0530

    CurrencyConvertor

--- CurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 
+
 function useCurrencyInfo(currency){
+    const [data, setDate] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-)
-    },[])
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setDate(res[currency]))
+        console.log(data);
+    },[currency])
+    return data
 }
+
+
