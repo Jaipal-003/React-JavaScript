e434bda25baada6fb404bd7269dec5c70d2206ac
Jaipal Singh Sisodiya <jaipalsinghsisodiya@gmail.com>
2024-07-29 21:54:53 +0530

    CurrencyConvertor

--- CurrencyConvertor/src/components/InputBox.jsx
+++ CurrencyConvertor/src/components/InputBox.jsx
@@ -2,17 +2,27 @@ import React from "react";
 
 function InputBox({
   label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOption = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
 
   className = "",
 }) {
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex  ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">label</label>
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
         <input
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
