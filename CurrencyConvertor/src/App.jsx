efe279a3d4e574fe8257f1459f02c55ac1b520fc
Jaipal Singh Sisodiya <jaipalsinghsisodiya@gmail.com>
2024-07-30 13:57:52 +0530

    CurrencyConvertor

--- CurrencyConvertor/src/App.jsx
+++ CurrencyConvertor/src/App.jsx
@@ -26,13 +26,12 @@ function App() {
 
   return (
     <div
-      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat font-mono"
       style={{
         backgroundImage: `url('https://img.freepik.com/free-photo/billie-dollar-with-business-chart_1150-750.jpg?w=1060&t=st=1722320167~exp=1722320767~hmac=6dca7a96054b596469e37798acb1e630f925b4792082f0404254f92290eb744c')`,
-      }}
-    >
-      <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+      }}>
+      <div className="w-full font-mono">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white\/30 ">
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -68,7 +67,7 @@ function App() {
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg font-mono"
             >
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
