6f26da4e0f9ffce7e4e49065f2b34d2d854abe20
Jaipal Singh Sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-08-06 23:08:12 +0530

    ReduxToolKit

--- ReduxToolKit/src/features/todo/todoSlice.js
+++ ReduxToolKit/src/features/todo/todoSlice.js
@@ -9,7 +9,22 @@ export const todoSlice = createSlice({
     name : 'todo',
     initialState, 
     reducers: {
-        addTodo : () => {},
-        removeTodo : () => {},
+        addTodo : (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+
+        },
+        removeTodo : (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
     }
 })
+
+export const {addTodo, removeTodo} = todoSlice.actions
+ 
+
+
+
