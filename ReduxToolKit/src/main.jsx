472d3e55ad627d61133cdb08cc2996c879670285
Jaipal Singh Sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-08-11 21:09:01 +0530

    Wrong

--- ReduxToolKit/src/main.jsx
+++ ReduxToolKit/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Provider} from 'react-redux'
-
-
 import App from './App.jsx'
 import './index.css'
 
-import { store } from './app/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
 )
