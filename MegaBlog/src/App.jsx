f139be83a7a9a31b6e063eede5e2ef4493eaaa52
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-08-14 23:09:29 +0530

    BlogApp

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
-     
+     <h1>A blog app with Appwrite</h1>
     </>
   )
 }
