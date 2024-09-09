d172ec767121bf26f1eeb46f7339d90718822178
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-09-09 21:50:29 +0530

    Recreate Mega Blog App

---
+++ MegaBlog/src/conf/config.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_COLLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_DATABASE_ID)
+};
+
+
+export default conf
