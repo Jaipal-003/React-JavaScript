87998293cc9c10e1ca95bdbf3d8008110c08c4f3
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-09-13 21:54:19 +0530

    set up environment

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,17 +1,13 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+
   return (
     <>
-    <h1>Blog App with Appwrite
-      
-    </h1>
-      
+      <h1>Blog App with Appwrite</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
