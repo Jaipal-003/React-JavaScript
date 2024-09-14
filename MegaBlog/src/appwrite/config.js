f03df2be60618b7a4c9c2476f2af95ff4985f8ee
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-09-14 21:29:36 +0530

    Database Config Setup

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -9,6 +9,22 @@ constructor(){
     this.client
         .setEndpoint(conf.appwriteUrl)
         .setProject (conf.appwriteProjectId);
+       this.databases = new Databases(this.client);
+}
+
+async createdPost({title, slug, content, featuredImage, status, userId}){
+    try {
+        return await this.databases.createDocument(conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,content,featuredImage,status,userId
+            }
+        )
+    } catch (error) {
+        console.log("Appwite serive :: createdPost :: error", error);
+    }
+
 }
 }
 
