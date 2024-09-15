60c071b70ebe230b8b7853874d269a485a139fcc
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-09-15 12:52:36 +0530

    Database Config Setup

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -49,7 +49,61 @@ export class Service {
       console.log("Appwrite serive :: updatePost :: error", error);
     }
   }
-}
+
+  import conf from "../conf.js";
+  import { Client, ID, Databases, Storage, Query } from "appwrite";
+  
+  export class Service {
+    client = new Client();
+    databases;
+    bucket;
+    constructor() {
+      this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+      this.databases = new Databases(this.client);
+    }
+  
+    async createdPost({ title, slug, content, featuredImage, status, userId }) {
+      try {
+        return await this.databases.createDocument(
+          conf.appwriteDatabaseId,
+          conf.appwriteCollectionId,
+          slug,
+          {
+            title,
+            content,
+            featuredImage,
+            status,
+            userId,
+          }
+        );
+      } catch (error) {
+        console.log("Appwite serive :: createdPost :: error", error);
+        throw error; // You can return a custom error if preferred
+      }
+    }
+  
+    async updatePost(slug, { title, content, featuredImage, status }) {
+      try {
+        return await this.databases.updateDocument(
+          conf.appwriteDatabaseId,
+          conf.appwriteCollectionId,
+          slug,
+          {
+            title,
+            content,
+            featuredImage,
+            status,
+          }
+        );
+      } catch (error) {
+        console.log("Appwrite serive :: updatePost :: error", error);
+      }
+    }
+  } 
+
+
 
 const service = new Service();
 export default service;
