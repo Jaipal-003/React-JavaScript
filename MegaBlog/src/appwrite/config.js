202c4ae420b0bca2927db5f836285f541b207341
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-09-15 12:37:59 +0530

    Database Config Setup

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -32,25 +32,23 @@ export class Service {
     }
   }
 
-  async updatePost (slug,{ title,
-    content,
-    featuredImage,
-    status,
-    }){
-        try {
-            return await this.databases.updateDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug,{
-                    title,content,featuredImage,status,
-                }
-            );
-        } catch (error) {
-            console.log("Appwrite serive :: updatePost :: error",error);
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
         }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: updatePost :: error", error);
     }
-
-
+  }
 }
 
 const service = new Service();
