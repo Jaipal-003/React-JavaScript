3779d33de017471692044ed0d05366d80ea10ed0
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-09-15 12:08:13 +0530

    Database Config Setup

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -1,33 +1,37 @@
-import conf from "../conf.js";     
-import { Client, ID,Databases, Storage, Query} from "appwrite";
+import conf from "../conf.js";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service{
-client = new Client();
-databases;
-bucket;
-constructor(){
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
     this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject (conf.appwriteProjectId);
-       this.databases = new Databases(this.client);
-}
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+  }
 
-async createdPost({title, slug, content, featuredImage, status, userId}){
+  async createdPost({ title, slug, content, featuredImage, status, userId }) {
     try {
-        return await this.databases.createDocument(conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug,
-            {
-                title,content,featuredImage,status,userId
-            }
-        )
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
     } catch (error) {
-        console.log("Appwite serive :: createdPost :: error", error);
-        throw error; // You can return a custom error if preferred 
+      console.log("Appwite serive :: createdPost :: error", error);
+      throw error; // You can return a custom error if preferred
     }
-
+  }
 }
-}
 
-const service = new Service()
-export default service
+const service = new Service();
+export default service;
