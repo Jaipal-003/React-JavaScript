b31fecef5be87ef71b61f38e186ce97bdb432bfe
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-11-05 14:40:29 +0530

    Database Config Setup

--- Mega Blog App/src/appwrite/auth.js
+++ Mega Blog App/src/appwrite/auth.js
@@ -1,46 +1,54 @@
-import conf from '../conf.js'; 
+import conf from "../conf.js";
 import { Client, Account, ID } from "appwrite";
 
-
 export class AuthService {
-    client =  new this.client();
-    account;
-
-    constructor(){
-        this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+    constructor() {
+        this.client = new Client()
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
 
     async createAccount({ email, password, name }) {
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
-            
             if (userAccount) {
-                // Call another method or perform further operations
+                // Call another method
+                return this.login({ email, password });
             } else {
-                return userAccount;  // Return null if userAccount is not created
+                return userAccount; // Return null if userAccount is not created
             }
         } catch (error) {
             throw error;
         }
     }
 
-
-    async login ({email,password}){
+    async login({ email, password }) {
         try {
-           return await this.account.createEmailSession(email,password);
-            
+            return await this.account.createEmailPasswordSession(email, password);
         } catch (error) {
             throw error;
         }
     }
 
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.error("Appwrite service :: getCurrentUser :: error", error);
+            return null;
+        }
+    }
 
-
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.error("Appwrite service :: logout :: error", error);
+        }
+    }
 }
 
-
+// Create an instance of AuthService
 const authService = new AuthService();
 export default authService;
