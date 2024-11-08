e0871a87dc3dbde033aae94c025bdecaa098b8d9
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-11-08 12:14:29 +0530

    Add Queries on the config js

--- Mega Blog App/src/appwrite/config.js
+++ Mega Blog App/src/appwrite/config.js
@@ -73,8 +73,17 @@ export class Service{
         }
     } 
 
-    async getPosts(queries = [Query.equal("status", "equal")] ){
-        
+    async getPosts(queries = [Query.equal("status", "active")] ){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false;
+        }
     }
 
 };
