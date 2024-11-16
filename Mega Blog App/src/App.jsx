e1cdb1fea55ce427871254b17e96e440537e397c
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-11-16 12:08:05 +0530

    REDUX setup on login & logout

--- Mega Blog App/src/App.jsx
+++ Mega Blog App/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
 
 function App() {
-const [loading, setLoading] =  useState(true)
-
-
-
-
-
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <h1>A blog app with appwrite</h1> 
+      <h1>A blog app with appwrite</h1>
     </>
   );
 }
