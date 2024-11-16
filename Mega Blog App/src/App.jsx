ea715ad46cdd1550f2b9cb4de9a4848a30ce521e
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-11-16 11:54:52 +0530

    REDUX setup on login & logout

--- Mega Blog App/src/App.jsx
+++ Mega Blog App/src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+const [loading, setLoading] =  useState(true)
+
+
+
+
+
   return (
     <>
       <h1>A blog app with appwrite</h1> 
