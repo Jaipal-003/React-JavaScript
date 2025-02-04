bd16728c8947e0dcdbd286ab65571075cf149fa5
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2025-02-04 21:15:15 +0530

    Header nav set

---
+++ Mega Blog App/src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Button({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "white",
+  className = "",
+  ...props
+}) {
+  return (
+    <Button
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor}
+        ${className} `} {...props}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default Button;
---
+++ Mega Blog App/src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React, {useId} from 'react'
+
+function Input({
+    
+
+}) {
+    const id = useId()
+  return (
+    <div>Input</div>
+  )
+}
+
+export default Input
