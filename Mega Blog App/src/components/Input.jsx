a2736f9bd7dff72ac5d42028052b595ada0ab023
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2025-02-05 13:23:53 +0530

    Header nav set

--- Mega Blog App/src/components/Input.jsx
+++ Mega Blog App/src/components/Input.jsx
@@ -4,16 +4,23 @@ const Input = React.forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref
 ) {
-  const id = useId()
+  const id = useId();
   return (
     <div className="w-full">
-      {label && 
+      {label && (
         <label className="inline-block md-1 pl-1" htmlFor={id}>
           {label}
         </label>
-      
-      }
-    </div> 
+      )}
+      <input
+        type={text}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full   ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+      />
+    </div>
   );
 });
+
 export default Input;
