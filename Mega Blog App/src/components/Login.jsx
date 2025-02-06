137700656bdbea61e86348f485dafb14ef930c66
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2025-02-06 14:25:55 +0530

    setup components

--- Mega Blog App/src/components/Login.jsx
+++ Mega Blog App/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
       if (session) {
         const userData = await authService.getCurrentUser();
         if (userData) dispatch(authLogin(userData));
-        navigate("/")
+        navigate("/");
       } else {
       }
     } catch (error) {
@@ -25,7 +25,22 @@ function Login() {
     }
   };
 
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign in to your account
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
