323fb7bd35e1f05c69574cc8578362c7a8a6f546
Jaipal singh sisodiya <jaipalsinghsisodiya46@gmail.com>
2025-02-06 14:17:58 +0530

    setup components

---
+++ Mega Blog App/src/components/Login.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Login as authLogin } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { useDispatch } from "react-redux";
+import authService from "../appwrite/auth";
+
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(authLogin(userData));
+      } else {
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return <div>Login</div>;
+}
+
+export default Login;
